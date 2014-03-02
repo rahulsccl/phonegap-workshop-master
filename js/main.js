b8d268da6071ed5121132da7cc0bc66ba99fd9b6
Rahul Attuluri <rahulsccl@gmail.com>
2014-03-03 02:19:45 +0530

    used handlebar.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+findByName: function() {
+    var self = this;
+    this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+    });
+},
 
     initialize: function() {
+        var self = this;
         this.store = new MemoryStore(function() {
-        self.showAlert('Store Initialized', 'Info');
-    });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+            self.renderHomeView();
+        });
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     },
 
     showAlert: function (message, title) {
@@ -26,6 +22,11 @@ var app = {
         } else {
             alert(title ? (title + ": " + message) : message);
         }
+    },
+
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
 };
