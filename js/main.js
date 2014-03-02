f0a3bd6d3c91dd536a09eed8f503bf0c88d2a36b
Rahul Attuluri <rahulsccl@gmail.com>
2014-03-03 02:10:41 +0530

    Added showalert changes

--- js/main.js
+++ js/main.js
@@ -14,9 +14,19 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        this.store = new MemoryStore(function() {
+        self.showAlert('Store Initialized', 'Info');
+    });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    },
+
+    showAlert: function (message, title) {
+       if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
 };
 
