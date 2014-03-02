19d19b606e18738d7a84cc2242057e93d7507996
Rahul Attuluri <rahulsccl@gmail.com>
2014-03-03 02:41:50 +0530

    Level 1 Code.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,21 @@
 var app = {
 
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
-
     findByName: function() {
-        var self = this;
+        console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
         });
-    },
-
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     initialize: function() {
-        var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        this.store = new MemoryStore(function() {
-            self.renderHomeView();
-        });
+        this.store = new MemoryStore();
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
