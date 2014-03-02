0a752cfc967dee0e3576fa5e6d3c2bcef83abee3
Rahul Attuluri <rahulsccl@gmail.com>
2014-03-03 02:25:30 +0530

    Given code level 5

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 var app = {
 
-findByName: function() {
-    var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
-    });
-},
-
-    initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function() {
-            self.renderHomeView();
-        });
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-    },
-
     showAlert: function (message, title) {
-       if (navigator.notification) {
+        if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
         }
     },
 
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+
     renderHomeView: function() {
         $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
+
+    initialize: function() {
+        var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.store = new MemoryStore(function() {
+            self.renderHomeView();
+        });
+    }
 
 };
 
